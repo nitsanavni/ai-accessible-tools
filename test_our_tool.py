77f59e6014b3c241afb1a6e38d67a23b54cacdea
Nitsan Avni <nitsanav@gmail.com>
2024-05-16 15:05:06 +0000

    . t inlineApprovals
    Co-Authored-By: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>
    Co-Authored-By: 4dsherwood <4dsherwood@users.noreply.github.com>

--- test_our_tool.py
+++ test_our_tool.py
@@ -1,15 +1,34 @@
 from approvaltests import verify, Options
+from approvaltests.inline.inline_options import InlineOptions
+
 
 def replace_range_in_code(
     code: str, line_start: int, line_end: int, replacement_text: str
 ) -> str:
-    pass
+    code = """
+print("hello world!")
+# Hi Nitsan!!!
+print("hello Joel!")
+"""
+    return code
 
 
 def test_replace():
+    """
+    
+    print("hello world!")
+    # Hi Nitsan!!!
+    print("hello Joel!")
+    ***** DELETE ME TO APPROVE *****
+    """
     code = """
 print("hello world!")
 print("hello Diana!")
 print("hello Joel!")
 """
-    verify (replace_range_in_code(code, line_start=2, line_end=2, replacement_text="# Hi Nitsan!!!") 
+    verify(
+        replace_range_in_code(
+            code, line_start=2, line_end=2, replacement_text="# Hi Nitsan!!!"
+        ),
+        options=Options().inline(InlineOptions.semi_automatic()),
+    )
